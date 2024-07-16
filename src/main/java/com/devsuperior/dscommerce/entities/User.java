4f8d986ef080ea40ce0f08c13e168ca298d5a3d9
GustavoLyra23 <gustavolyra23@gmail.com>
2024-07-16 04:32:34 -0300

    many-to-many product and category

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -15,6 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String phone;
     private LocalDate birthdate;
