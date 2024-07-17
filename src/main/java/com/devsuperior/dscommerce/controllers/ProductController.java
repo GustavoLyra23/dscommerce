5f70145ddcd2189221646d681887e3db8990990b
GustavoLyra23 <gustavolyra23@gmail.com>
2024-07-17 09:59:22 -0300

    GET PRoducts paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDto;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,11 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDto fidById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDto> fidAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscommerce.dto.ProductDto;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +22,12 @@ public class ProductService {
         Optional<Product> result = productRepository.findById(id);
         return new ProductDto(result.get());
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDto> findAll(Pageable pageable) {
+        Page<Product> products = productRepository.findAll(pageable);
+        return products.map(ProductDto::new);
+    }
+
 
 }
