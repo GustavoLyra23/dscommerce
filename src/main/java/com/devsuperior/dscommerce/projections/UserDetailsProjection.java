c4c4b01e50dcdc533f3ffdf5b441e54af964b543
GustavoLyra23 <gustavolyra23@gmail.com>
2024-07-24 11:31:08 -0300

    UserDetailsService

---
+++ src/main/java/com/devsuperior/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+
+}
