5e4adf754cacd27e567649501124a1ae800c0f41
GustavoLyra23 <gustavolyra23@gmail.com>
2024-07-24 10:31:52 -0300

    Domain model User Role, seed

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -20,6 +18,11 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
@@ -114,4 +117,16 @@ public class User {
         return orders;
     }
 
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        return roles.contains(roleName);
+    }
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
 }
